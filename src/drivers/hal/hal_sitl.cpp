dcd02b6dc1a81acdff350982bfc1eb41e5a62aa2
r3ike <rpogliacomi@gmail.com>
2025-11-15 12:56:31 +0100

    - continuo lavoro sull'HAL per SITL

---
+++ src/drivers/hal/hal_sitl.cpp
@@ -0,0 +1,12 @@
+#include <drivers/hal/hal.hpp>
+
+class HAL_IMU_SITL : public HAL_IMU {
+public:
+    void readGyro() override {
+
+    }
+
+    void readAccel() override {
+        
+    }
+};
---
+++ src/drivers/hal/hal_teensy.cpp
@@ -0,0 +1,16 @@
+#include <drivers/hal/hal.hpp>
+
+class HAL_IMU_teensy : public HAL_IMU
+{
+private:
+    /* data */
+public:
+    HAL_IMU_teensy(){
+
+    };
+
+    void readGyro() override{
+
+    }
+};
+

7b7eac8c6c9f5def11bb8047b3d1e66066f285f5
r3ike <rpogliacomi@gmail.com>
2025-11-20 14:03:45 +0100

    - continuo wrapper gps

--- src/drivers/gps/Gps.cpp
+++ src/drivers/gps/Gps.cpp
@@ -4,6 +4,31 @@
 Gps::Gps(){}
 Gps::~Gps() {}
 
-void Gps::init()
+void Gps::init(Stream *serial)
 {
+    serialPtr = serial;
+
+    // --- UBX CONFIG ---
+    sendUBX(UBX_CFG_RATE_10HZ, sizeof(UBX_CFG_RATE_10HZ));
+
+    sendUBX(UBX_DISABLE_GLL, sizeof(UBX_DISABLE_GLL));
+    sendUBX(UBX_DISABLE_GSA, sizeof(UBX_DISABLE_GSA));
+    sendUBX(UBX_DISABLE_GSV, sizeof(UBX_DISABLE_GSV));
+    sendUBX(UBX_DISABLE_VTG, sizeof(UBX_DISABLE_VTG));
+}
+
+void Gps::read(){
+    while (serialPtr->available())
+    {
+        gps.encode(serialPtr->read());
+    }
+}
+
+/*-------------------
+    Private method
+--------------------*/
+void Gps::sendUBX(const uint8_t *msg, uint8_t len)
+{
+    serialPtr->write(msg, len);
+    serialPtr->flush();
 }
